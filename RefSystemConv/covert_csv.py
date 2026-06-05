b8bf332a4f20d6552d11c1b7a158b3b54bf5ce8b
lcmrl <lcmorelli.eng@gmail.com>
2026-06-05 17:54:01 +0200

    minor changes

--- RefSystemConv/covert_csv.py
+++ RefSystemConv/covert_csv.py
@@ -1,43 +1,71 @@
-# >conda create --name pyproj_env
-# >conda activate pyproj_env
-# >conda install -c conda-forge pyproj
-# >conda deactivate pyproj_env
-
 import pymap3d as pm
 from pyproj import Transformer
 
-WORKING_DIR = r"./"
+WORKING_DIR = "./"
 
-INPUT_COORD_FILE = r"{}/input.txt".format(WORKING_DIR)
-OUTPUT_FILE_1 = r"{}/converted_LatLonAlt.txt".format(WORKING_DIR)
-OUTPUT_FILE_2 = r"{}/converted_ENU.txt".format(WORKING_DIR)
-file1 = open(OUTPUT_FILE_1, "w")
-file2 = open(OUTPUT_FILE_2, "w")
+INPUT_COORD_FILE = f"{WORKING_DIR}/input2.txt"
+OUTPUT_FILE_1 = f"{WORKING_DIR}/converted_LatLonAlt.txt"
+OUTPUT_FILE_2 = f"{WORKING_DIR}/converted_ENU.txt"
 
-input_system  = 'epsg:4978' # WGS84 geocentric 'epsg:4978'
-output_system = 'epsg:4326' # WGS84 geographic 'epsg:4326'
-enu_reference_point = (4347718.9572, 856411.9883, 4573643.2406) # ECEF coordinates of MOCA in WGS84 geocentric 'epsg:4978'
+input_system  = "epsg:4978"   # ECEF WGS84
+output_system = "epsg:4326"   # Geodetic WGS84
 
-### MAIN ###
-transformer = Transformer.from_crs(input_system, output_system)
-transform_object = transformer.itransform([(enu_reference_point[0], enu_reference_point[1], enu_reference_point[2])])
-for pt in transform_object:
-    enu_reference = (pt[0], pt[1], pt[2])
-    print("enu_reference", enu_reference)
+# Reference point in ECEF
+enu_reference_point = (
+    4347718.9572,
+    856411.9883,
+    4573643.2406
+)
 
-c = 0
-with open(INPUT_COORD_FILE, "r") as file:
-    for line in file:
+# IMPORTANT
+transformer = Transformer.from_crs(
+    input_system,
+    output_system,
+    always_xy=True
+)
+
+# Convert reference ECEF -> geodetic
+lon0, lat0, h0 = transformer.transform(
+    enu_reference_point[0],
+    enu_reference_point[1],
+    enu_reference_point[2]
+)
+
+print("Reference:")
+print(lat0, lon0, h0)
+
+with open(OUTPUT_FILE_1, "w") as file1, \
+     open(OUTPUT_FILE_2, "w") as file2, \
+     open(INPUT_COORD_FILE, "r") as infile:
+
+    for line in infile:
+
         line = line.strip()
-        id, X, Y, Z = line.split(None, 4)
+
+        if not line:
+            continue
+
+        id_, X, Y, Z, _, _, _, _, = line.split(" ", 7)
+
         X = float(X)
         Y = float(Y)
         Z = float(Z)
 
-        transform_object = transformer.itransform([(X, Y, Z)])
-        for pt in transform_object:
-            Xc, Yc, Zc = pt[0], pt[1], pt[2]
-            file1.write("{} {} {} {}\n".format(id, Xc, Yc, Zc))
-        
-        enuX, enuY, enuZ = pm.geodetic2enu(Xc, Yc, Zc, enu_reference[0], enu_reference[1], enu_reference[2])
-        file2.write("{} {} {} {}\n".format(id, enuX, enuY, enuZ))
+        # ECEF -> geodetic
+        lon, lat, h = transformer.transform(X, Y, Z)
+
+        file1.write(f"{id_} {lat} {lon} {h}\n")
+
+        # geodetic -> ENU
+        east, north, up = pm.geodetic2enu(
+            lat,
+            lon,
+            h,
+            lat0,
+            lon0,
+            h0
+        )
+
+        file2.write(f"{id_} {east} {north} {up}\n")
+
+        print(lat, lon, h)
